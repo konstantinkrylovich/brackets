01fba27937953c8672159bbb4c0d60cdce99951d
konstantinkrylovich <konstantinkrylovich@gmail.com>
2019-03-17 21:11:18 +0300

    brackets/feat: implement algorithm due to stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  class Stack {
+    constructor() {
+      this.collection = [];
+    }
+    push(val) {
+      return this.collection.push(val);
+    }
+    pop() {
+      return this.collection.pop();
+    }
+    peek() {
+      return this.collection[this.collection.length - 1];
+    }
+    isEmpty() {
+      return this.collection.length === 0;
+    }
+    clear() {
+      return this.collection.length = 0;
+    }
+  }
+
+  stackBrackets = new Stack();
+  let brackets = str.split('');
+  debugger;
+  for(let i = 0; i < brackets.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if (brackets[i] == bracketsConfig[j][0]) {
+        if (brackets[i] == bracketsConfig[j][1] && !stackBrackets.isEmpty()) {
+          let lastBracket = stackBrackets.pop();
+          if (lastBracket == bracketsConfig[j][0]) {
+            break;
+          } else {
+            stackBrackets.push(lastBracket);
+            stackBrackets.push(brackets[i]);
+          }
+        } else{
+          stackBrackets.push(brackets[i]);
+          break;
+        }
+        
+      } else if (brackets[i] == bracketsConfig[j][1]) {
+        let lastBracket = stackBrackets.pop();
+        if (lastBracket == bracketsConfig[j][0]) {
+          break;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  if (stackBrackets.isEmpty()) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
+
